f00a87244b40d14154f3338c405896c147f3a154
BenGRey5 <bgraynolds@gmail.com>
2023-12-08 10:33:38 -0800

    coffeeShop

--- help-ueue/src/components/coffeeShop.js
+++ help-ueue/src/components/coffeeShop.js
@@ -1,1 +1,17 @@
-thththththhggdfd
+import React from 'react';
+
+function Item({ item, onBuy, onRestock, onDelete}) {
+    return (
+        <div>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+            <p>{item.quantity > 0 ? `Quantity: ${item.quantity}` : 'Out of Stock'}</p>
+<button onClick={() => onBuy(item.id)}>Buy</button>
+<button onClick={() => onRestock(item.id, 1)}
+
+
+
+        </div>
+
+    )
+}
