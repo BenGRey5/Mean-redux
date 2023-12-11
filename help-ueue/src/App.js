f44a9a0eb5f1350e76a7df45e8747181b5f59b57
BenGRey5 <bgraynolds@gmail.com>
2023-12-11 05:35:52 -0800

    WIP

--- help-ueue/src/App.js
+++ help-ueue/src/App.js
@@ -1,60 +1,13 @@
-// App.js
-import React, { useState } from 'react';
-import ItemList from './components/coffeeList.js';
+import React from 'react';
 import OptionList from './components/userOptions.js';
+import Form1 from './components/Form1.js';
 
 function App() {
-  const [items, setItems] = useState([
-    { id: 1, name: 'Arabica Light Roast', description: 'Imported from Nicaragua', quantity: 130 },
-    { id: 2, name: 'Robusta Medium Roast', description: 'Imported from Brazil', quantity: 130 },
-    { id: 3, name: 'Liberica Light Roast', description: 'Imported from Philippines', quantity: 130 },
-    { id: 4, name: 'Excelsa Dark Roast', description: 'Imported from South America', quantity: 130 },
-  ]);
-
-  const [showInventory, setShowInventory] = useState(true);
-
-  const addItem = (newItem) => {
-    setItems(prevItems => [...prevItems, { ...newItem, id: Date.now() }]);
-  };
-
-  const updateItem = (itemId, updatedItem) => {
-    setItems(prevItems => prevItems.map(item =>
-      item.id === itemId ? { ...item, ...updatedItem } : item
-    ));
-  };
-
-  const deleteItem = (itemId) => {
-    setItems(prevItems => prevItems.filter(item => item.id !== itemId));
-  };
-
-  const sellItem = (itemId) => {
-    setItems(prevItems => prevItems.map(item =>
-      item.id === itemId ? { ...item, quantity: Math.max(item.quantity - 1, 0) } : item
-    ));
-  };
-
-  const restockItem = (coffeeDetails) => {
-    setShowInventory(false); // Hide current inventory when restocking
-    // Add a new item for each restock option
-    setItems(prevItems => [...prevItems, { ...coffeeDetails, id: Date.now() }]);
-  };
-
-  const handleCancelRestock = () => {
-    setShowInventory(true); // Show inventory when restock is canceled
-  };
-
   return (
     <div className="App">
       <h1>The Mean Bean</h1>
-      <OptionList onAddItem={restockItem} onCancelRestock={handleCancelRestock} />
-      {showInventory && (
-        <ItemList
-          items={items}
-          onSell={sellItem}
-          onDelete={deleteItem}
-          onUpdate={updateItem}
-        />
-      )}
+      <OptionList onAddItem={(newItem) => console.log(newItem)} />
+      <Form1 />
     </div>
   );
 }
--- help-ueue/src/components/userOptions.js
+++ help-ueue/src/components/userOptions.js
@@ -1,23 +1,12 @@
-// userOptions.js
+
 import React, { useState } from 'react';
 
 function OptionList({ onAddItem }) {
     const [selectedOption, setSelectedOption] = useState('');
-    const [showForm, setShowForm] = useState(false);
-    const [showInventory, setShowInventory] = useState(true);
 
     const handleRadioChange = (event) => {
         const selectedCoffee = event.target.value;
         setSelectedOption(selectedCoffee);
-    };
-
-    const handleRestockButtonClick = () => {
-        setShowForm(true);
-        setShowInventory(false);
-    };
-
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
 
         // Assuming each coffee type has specific details
         const coffeeDetails = {
@@ -27,88 +16,138 @@ function OptionList({ onAddItem }) {
             'Excelsa': { name: 'Excelsa Dark Roast', description: 'Imported from South America', quantity: 130 },
         };
 
-        // Add the corresponding item when the form is submitted
-        onAddItem(coffeeDetails[selectedOption]);
-
-        // Reset state and show the inventory
-        setSelectedOption('');
-        setShowForm(false);
-        setShowInventory(true);
-    };
-
-    const handleFormCancel = () => {
-        // Reset state and show the inventory when canceled
-        setSelectedOption('');
-        setShowForm(false);
-        setShowInventory(true);
+        // Add the corresponding item when a radio button is selected
+        onAddItem(coffeeDetails[selectedCoffee]);
     };
 
     return (
         <div>
-            {showInventory && (
-                <div>
-                    <h2>Select Coffee for Restock</h2>
-                    <button onClick={handleRestockButtonClick}>Restock</button>
-                    <div>
-                        Selected option: {selectedOption}
-                    </div>
-                </div>
-            )}
+            <h2>Select Coffee</h2>
+            <label>
+                <input
+                    type="radio"
+                    value="Arabica"
+                    name="coffeeOptions"
+                    checked={selectedOption === 'Arabica'}
+                    onChange={handleRadioChange}
+                />
+                Arabica
+            </label>
 
-            {showForm && (
-                <form onSubmit={handleFormSubmit}>
-                    <label>
-                        <input
-                            type="radio"
-                            value="Arabica"
-                            name="coffeeOptions"
-                            checked={selectedOption === 'Arabica'}
-                            onChange={handleRadioChange}
-                        />
-                        Arabica
-                    </label>
+            <label>
+                <input
+                    type="radio"
+                    value="Robusta"
+                    name="coffeeOptions"
+                    checked={selectedOption === 'Robusta'}
+                    onChange={handleRadioChange}
+                />
+                Robusta
+            </label>
 
-                    <label>
-                        <input
-                            type="radio"
-                            value="Robusta"
-                            name="coffeeOptions"
-                            checked={selectedOption === 'Robusta'}
-                            onChange={handleRadioChange}
-                        />
-                        Robusta
-                    </label>
+            <label>
+                <input
+                    type="radio"
+                    value="Liberica"
+                    name="coffeeOptions"
+                    checked={selectedOption === 'Liberica'}
+                    onChange={handleRadioChange}
+                />
+                Liberica
+            </label>
 
-                    <label>
-                        <input
-                            type="radio"
-                            value="Liberica"
-                            name="coffeeOptions"
-                            checked={selectedOption === 'Liberica'}
-                            onChange={handleRadioChange}
-                        />
-                        Liberica
-                    </label>
-
-                    <label>
-                        <input
-                            type="radio"
-                            value="Excelsa"
-                            name="coffeeOptions"
-                            checked={selectedOption === 'Excelsa'}
-                            onChange={handleRadioChange}
-                        />
-                        Excelsa
-                    </label>
-
-                    <div>
-                        <button type="submit">Submit</button>
-                        <button type="button" onClick={handleFormCancel}>Cancel</button>
-                    </div>
-                </form>
-            )}
+            <label>
+                <input
+                    type="radio"
+                    value="Excelsa"
+                    name="coffeeOptions"
+                    checked={selectedOption === 'Excelsa'}
+                    onChange={handleRadioChange}
+                />
+                Excelsa
+            </label>
+            <div>
+                Selected option: {selectedOption}
+            </div>
         </div>
     );
 }
 
 export default OptionList;
+
+
+// import React, { useState } from 'react';
+
+// function OptionList({ onAddItem }) {
+//     const [selectedOption, setSelectedOption] = useState('');
+
+//     const handleRadioChange = (event) => {
+//         const selectedCoffee = event.target.value;
+//         setSelectedOption(selectedCoffee);
+
+//         // Assuming each coffee type has specific details
+//         const coffeeDetails = {
+//             'Arabica': { name: 'Arabica Light Roast', description: 'Imported from Nicaragua', quantity: 130 },
+//             'Robusta': { name: 'Robusta Medium Roast', description: 'Imported from Brazil', quantity: 130 },
+//             'Liberica': { name: 'Liberica Light Roast', description: 'Imported from Philippines', quantity: 130 },
+//             'Excelsa': { name: 'Excelsa Dark Roast', description: 'Imported from South America', quantity: 130 },
+//         };
+
+//         // Add the corresponding item when a radio button is selected
+//         onAddItem(coffeeDetails[selectedCoffee]);
+//     };
+
+//     return (
+//         <div>
+//             <h2>Select Coffee</h2>
+//             <label>
+//                 <input
+//                     type="radio"
+//                     value="Arabica"
+//                     name="coffeeOptions"
+//                     checked={selectedOption === 'Arabica'}
+//                     onChange={handleRadioChange}
+//                 />
+//                 Arabica
+//             </label>
+
+//             <label>
+//                 <input
+//                     type="radio"
+//                     value="Robusta"
+//                     name="coffeeOptions"
+//                     checked={selectedOption === 'Robusta'}
+//                     onChange={handleRadioChange}
+//                 />
+//                 Robusta
+//             </label>
+
+//             <label>
+//                 <input
+//                     type="radio"
+//                     value="Liberica"
+//                     name="coffeeOptions"
+//                     checked={selectedOption === 'Liberica'}
+//                     onChange={handleRadioChange}
+//                 />
+//                 Liberica
+//             </label>
+
+//             <label>
+//                 <input
+//                     type="radio"
+//                     value="Excelsa"
+//                     name="coffeeOptions"
+//                     checked={selectedOption === 'Excelsa'}
+//                     onChange={handleRadioChange}
+//                 />
+//                 Excelsa
+//             </label>
+//             <div>
+//                 Selected option: {selectedOption}
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default OptionList;
