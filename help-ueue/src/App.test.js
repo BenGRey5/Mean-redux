2717c407a7e0775dcf2ffd3761813e20ab0ad7ee
BenGRey5 <146392527+BenGRey5@users.noreply.github.com>
2023-12-11 06:57:15 -0800

    Delete help-ueue directory

--- help-ueue/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import Item from './components/coffeeShop';
-
-test('renders learn react link', () => {
-  render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  expect(App).toBeInTheDocument();
-});
