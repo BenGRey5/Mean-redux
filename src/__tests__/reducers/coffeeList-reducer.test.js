b991af197d05ab9232303928a5c0fb3126427dfb
BenGRey5 <bgraynolds@gmail.com>
2023-12-15 08:59:15 -0800

    add redux

--- src/__tests__/reducers/coffeeList-reducer.test.js
+++ src/__tests__/reducers/coffeeList-reducer.test.js
@@ -1,4 +1,4 @@
-import coffeeListReducer from "../../reducers"
+import coffeeListReducer from "../reducers/coffeeListReducer.js"
 
 describe('coffeeListReducer', () => {
     test('Should return default state if there is no action type passed into the reducer', () => {
