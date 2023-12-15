ed5e96e8d04a98d684706eb10a960d267c51a684
BenGRey5 <bgraynolds@gmail.com>
2023-12-15 08:36:57 -0800

    add reducers

---
+++ src/__tests__/reducers/coffeeList-reducer.test.js
@@ -0,0 +1,7 @@
+import coffeeListReducer from "../../reducers"
+
+describe('coffeeListReducer', () => {
+    test('Should return default state if there is no action type passed into the reducer', () => {
+        expect(ticketListReducer({}, { type: null})).toEqual({});
+    });
+});
